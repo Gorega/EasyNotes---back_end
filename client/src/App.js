3eced7c32ad284a0c54c3b83dc3fb3c38f41419e
Wael <wael@Waels-Mac-mini.local>
2022-04-11 01:42:39 +0300

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
     <AppProvider>
       <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route element={<ProtectedRoutes />}>
