9db8c34456cf2025e41f24be4c29718f559a70a2
Wael <wael@Waels-Mac-mini.local>
2022-11-29 01:53:39 +0200

    render service URI

--- client/src/lib/config.js
+++ client/src/lib/config.js
@@ -1,2 +1,2 @@
 const dev = process.env.NODE_ENV !== `production`
-export const server = dev ? "http://localhost:8000" : "https://easynotes-gorega.onrender.com";
+export const server = dev ? "http://localhost:8000" : "https://easynotes-gorega.onrender.com" && "https://easynotes-gorega-preview.onrender.com";
