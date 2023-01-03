bcbd9f4f4016a374a630e5ffb58658b2445f1aae
Wael <wael@Waels-Mac-mini.local>
2023-01-03 19:01:58 +0200

    auto refresh access-token google auth mail

--- controllers/mail.js
+++ controllers/mail.js
@@ -7,43 +7,45 @@ const myOAuth2Client = new OAuth2(
   process.env.MAIL_CLIENT_SECRET,
   "https://developers.google.com/oauthplayground"
   )
+  
+  myOAuth2Client.setCredentials({
+    refresh_token:process.env.MAIL_REFRESH_TOKEN
+    });
 
-myOAuth2Client.setCredentials({
-  refresh_token:process.env.MAIL_REFRESH_TOKEN
-  });
-
-const myAccessToken = myOAuth2Client.getAccessToken()
+  const myAccessToken = myOAuth2Client.getAccessToken()
 
 const mail = (to,mailMessage,mainSubject) =>{
-      // send email
-      var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-              type: "OAuth2",
-              user: process.env.EMAIL_SERVICE,
-              clientId: process.env.MAIL_CLIENT_ID,
-              clientSecret: process.env.MAIL_CLIENT_SECRET,
-              refreshToken: process.env.MAIL_REFRESH_TOKEN,
-              accessToken: myAccessToken
-        }
-      });
-
-      var mailOptions = {
-        from: process.env.EMAIL_SERVICE,
-        to: to,
-        subject: mainSubject,
-        html: mailMessage
-      };
-
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          // Error handler
-          return;
-        } else {
-          // Email sent successfuly handler
-          return;
-        }
-      });
+      myAccessToken.then(res=> {
+        // send email
+        var transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+                type: "OAuth2",
+                user: process.env.EMAIL_SERVICE,
+                clientId: process.env.MAIL_CLIENT_ID,
+                clientSecret: process.env.MAIL_CLIENT_SECRET,
+                refreshToken: res.token,
+                accessToken: myAccessToken
+          }
+        });
+  
+        var mailOptions = {
+          from: process.env.EMAIL_SERVICE,
+          to: to,
+          subject: mainSubject,
+          html: mailMessage
+        };
+  
+        transporter.sendMail(mailOptions, function(error, info){
+          if (error) {
+            // Error handler
+            return;
+          } else {
+            // Email sent successfuly handler
+            return;
+          }
+        });
+      })
   }
 
 module.exports = mail;
